cbeb3f378bac1ee14beb1dd835137cb382b638ca
Omer Miloh <omerrob11@gmail.com>
2024-05-04 14:22:49 +0300

    finishin randomColor function

--- script2.js
+++ script2.js
@@ -10,9 +10,8 @@ function gridCreation () {
     for (let i = 1; i <= size*size; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.toggle("grid_item");
-        let definedWitdth = setWidth();
+        let definedWitdth = 100/size + "%"
         gridItem.style.width = definedWitdth;
-
 
         appendElement(gridContainer,gridItem);
         addOnHoverListener(gridItem);
@@ -27,23 +26,14 @@ function appendElement (container,child) {
     container.appendChild(child);
 }
 
-function setWidth() {
-    return 100/size + "%"
-}
-
-function createRandomColor() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    return "rgb(" + x + "," + y + "," + z + ")";  
+function setWidth(size) {
+    item.style.width = 100/size;
 }
 
 // Event listeneres
 function addOnHoverListener(item) {
     item.addEventListener("mouseover" , (e) => {
-        //item.classList.add("hover");
-        let randomColor = createRandomColor();
-        item.style.backgroundColor = randomColor;
+        item.classList.add("hover")
     })
 }
 
@@ -54,7 +44,7 @@ gridSizeBtn.addEventListener("click", (e) => {
     if (size >100) {
         size = 16;
     }
-    
+    debugger;
     gridCreation();
 
 })
@@ -75,8 +65,6 @@ gridCreation();
 // once it's getting "hoverd", it will then listen call the function, and will do what needed
 // meaning- it will only call the function , when you over - so when it does, you can remove it
 
-// but you need to add the event listner with an handle:
-// btn.addeventListener("click", handleFunction)
 /* function hoverAnElement (e) {
     e.target.classList.toggle("hover");
     e.target.removeEventListener("mouseover",hoverAnElement);
